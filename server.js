2dcaf8a5e73e43718211aa8cdee50713f6b01a57
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-03-11 04:08:06 +0530

    Connecting Mongodb Database
    bad auth error obtained on while connecting using Mongodb compass

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 
 //config env
 dotenv.config();
 
+//database config
+connectDB();
 
 //rest object
-
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get('/' , (req,res) => {
