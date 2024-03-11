ad5df285c1e93c300240def1c3b9c8f3a3e4901c
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-03-11 18:31:30 +0530

    Protected Middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -110,3 +110,8 @@ export const loginController = async(req, res) => {
         });
     }
 };
+
+//test controller
+export const testController = (req,res) => {
+    console.log('Protected Route');
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {registerController , loginController} from '../controllers/authController.js';
+import {registerController , loginController , testController} from '../controllers/authController.js';
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -10,6 +11,10 @@ router.post('/register' , registerController);
 
 //LOGIN || POST
 router.post('/login', loginController);
+
+//test Routes
+router.get('/test', requireSignIn ,isAdmin, testController);
+
 
 
 export default router;
