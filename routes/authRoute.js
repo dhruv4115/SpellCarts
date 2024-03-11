e440779f3a5d10d40354893fce7d9dc882d88641
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-03-11 17:31:45 +0530

    LOGIN API

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerController} from '../controllers/authController.js';
+import {registerController , loginController} from '../controllers/authController.js';
 
 //router object
 const router = express.Router();
@@ -7,6 +7,9 @@ const router = express.Router();
 //routing
 //REGISTER || POST
 router.post('/register' , registerController);
+
+//LOGIN || POST
+router.post('/login', loginController);
 
 
 export default router;
