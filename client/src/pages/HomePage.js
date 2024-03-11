374319e4e8e56cfb64bd6f590429c394b61244de
diti8765 <ditigupta03@gmail.com>
2024-03-11 17:50:27 +0530

    Your commit message here

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout.js';
+const HomePage = () => {
+  return (
+    <Layout>
+        <h1>HomePage
+        </h1>
+        </Layout>
+  )
+}
+
+export default HomePage
+
+//C:\Users\ditig\Desktop\INNODEV1\SpellCarts\SpellCarts\client\src\components\Layout\Layout.js
