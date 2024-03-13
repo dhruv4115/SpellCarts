8d8dedcff98101611f72958947e5a08853e997fe
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-03-14 05:26:10 +0530

    Admin and user panel created successfully

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -45,7 +45,7 @@ const handleLogout = () => {
             {auth?.user?.name}
           </NavLink>
           <ul className="dropdown-menu">
-            <li><NavLink to="/dashboard" className="dropdown-item">Dashboard</NavLink></li>
+            <li><NavLink to={`/dashboard/${auth?.user?.role ===1 ? 'admin' : 'user'}`} className="dropdown-item">Dashboard</NavLink></li>
             <li><NavLink onClick={handleLogout}to="/login" className="dropdown-item">Logout</NavLink></li>
           </ul>
   </li>
