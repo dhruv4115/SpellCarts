8f7986ffba455fa2470d98a13614e7d24113faba
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2024-03-14 03:12:22 +0530

    logout, private routes implemented, location history, redirect to spellcart, node modules & env to git ignore

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -2,7 +2,7 @@
 import React,{useState}from 'react';
 import Layout from './../../components/Layout/Layout';
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
 import {toast} from 'react-toastify';
 import "../../styles/AuthStyles.css";
 import {useAuth} from "../../context/auth";
@@ -12,6 +12,7 @@ const Login = () => {
     const [password,setPassword] = useState("")
     const [auth,setAuth]= useAuth()
     const navigate=useNavigate();
+    const location = useLocation();
     //form function
 const handleSubmit=async (e) =>{
     e.preventDefault();
@@ -25,7 +26,7 @@ const handleSubmit=async (e) =>{
         token:res.data.token,
     });
     localStorage.setItem("auth",JSON.stringify(res.data)); 
-    navigate("/");
+    navigate(location.state||"/");
 
    }else{
     toast.error(res.data.message);
