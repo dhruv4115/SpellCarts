14c33b0683adf11f918c6f2a65712904d9467273
diti8765 <ditigupta03@gmail.com>
2024-03-18 21:07:07 +0530

    gitignore

--- client/src/components/Forms/SearchInput.js
+++ client/src/components/Forms/SearchInput.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSearch } from "../../context/Search";
+import { useSearch } from "../../context/search";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const SearchInput = () => {
