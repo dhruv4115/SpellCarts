dc9b7402bfbe718d241b3124b2f485ee34aa3f9d
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2025-03-21 04:55:07 +0530

    deployment changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
+import path from 'path';
 
 //config env
 dotenv.config();
@@ -21,15 +22,14 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.static(path.join(__dirname, './client/build')));
 //routes
 app.use('/api/v1/auth' , authRoutes);
 app.use('/api/v1/category' , categoryRoutes);
 app.use('/api/v1/product' , productRoutes);
 //rest api
-app.get('/' , (req,res) => {
-    res.send({
-        message:'Welcome to Spellcart, MERN Stack Project'
-    });
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 //PORT
